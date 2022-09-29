6fb6f9e210910674ebdd34670bc81d53c177a68d
Pavel Makovetsky <morgan855873@gmail.com>
2022-09-29 15:33:09 +0300

    Initial commit

--- pattern/01_facade.go
+++ pattern/01_facade.go
@@ -1,7 +1,13 @@
-package pattern
+package main
+
+import "fmt"
 
 /*
 	Реализовать паттерн «фасад».
 Объяснить применимость паттерна, его плюсы и минусы,а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Facade_pattern
 */
+
+func main() {
+	fmt.Println("hello")
+}
--- pattern/02_builder.go
+++ pattern/02_builder.go
@@ -1,7 +1,14 @@
-package pattern
+package main
+
+import "fmt"
 
 /*
 	Реализовать паттерн «строитель».
+
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
+
 	https://en.wikipedia.org/wiki/Builder_pattern
 */
+func main() {
+	fmt.Println("world")
+}
