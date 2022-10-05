4711e20369d092004b9073d13eef15491d2762da
Pavel Makovetsky <morgan855873@gmail.com>
2022-10-05 16:41:44 +0300

    +dev07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -33,6 +38,43 @@ start := time.Now()
 fmt.Printf(“fone after %v”, time.Since(start))
 */
 
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	orDone := make(chan interface{})
+	for _, v := range channels {
+		//для каждого канала запускаем горутину, которая ждет когда закроется канал
+		go func(c <-chan interface{}) {
+			select {
+			//если хоть один закрылся, закрываем канал на выход, тем самым разблокируем его на чтение
+			case <-c:
+				close(orDone)
+			case <-orDone:
+				return
+			}
+		}(v)
+	}
+	return orDone
+}
+
 func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(1*time.Second),
+		sig(10*time.Second),
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("Done after %v", time.Since(start))
 
 }
